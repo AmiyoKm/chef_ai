a6dd879ae6ee8de283c1b93c92623bfb5293c23f
AmiyoKm <games.amiyo@gmail.com>
2025-01-25 04:57:42 +0600

    Refactor GenerateRecipes action for improved code readability and consistency

--- actions/GenerateRecipes.ts
+++ actions/GenerateRecipes.ts
@@ -5,28 +5,28 @@ import prisma from "@/lib/prisma"
 import { auth } from "@clerk/nextjs/server"
 import { recipe } from "@/prisma/generated/client"
 
-export async function GenerateRecipes(input : string){
-    const {userId} = await auth()
-    if(!userId){
+export async function GenerateRecipes(input: string) {
+    const { userId } = await auth()
+    if (!userId) {
         throw new Error("unauthenticated")
     }
 
-    const response   =await generateRecipesByAI(input)
-    
-    const recipes : recipe[]= response.recipes.map((recipe :any) => ({
+    const response = await generateRecipesByAI(input)
+
+    const recipes: recipe[] = response.recipes.map((recipe: any) => ({
         userId,
         title: recipe.title,
         ingredients: recipe.ingredients,
         instruction: recipe.Instruction,
         vegan: recipe.vegan,
         calorie: recipe.calorie,
-        tags: recipe.tags.join(', '), // Convert tags array to a comma-separated string
+        tags: recipe.tags.join(', ')
     }));
-    
-   await prisma.recipe.createMany({
-        data : recipes
+
+    await prisma.recipe.createMany({
+        data: recipes
     })
 
-   return recipes
+    return recipes
 
 }
