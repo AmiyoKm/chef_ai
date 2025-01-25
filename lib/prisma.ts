be3f32c8d273a22e8e243ab014660c190ab4bdb4
AmiyoKm <games.amiyo@gmail.com>
2025-01-25 15:15:17 +0600

    Refactor Prisma client import and update datasource to PostgreSQL

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,9 @@
-import { PrismaClient } from "@prisma/client"
+// lib/prisma.ts
+import { PrismaClient } from "@/prisma/generated/client";
 
-const prisma = new PrismaClient()
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-const globalForPrisma = global as unknown as { prisma: typeof prisma }
+export const prisma =
+  globalForPrisma.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
-
-export default prisma
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
